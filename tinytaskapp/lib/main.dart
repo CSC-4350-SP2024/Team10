10719e8e95330eee73500a468df7340e6e222d23
Raymond Hernandez <raymondhr12@gmail.com>
2024-02-16 19:00:32 -0500

    temp change to main

--- tinytaskapp/lib/main.dart
+++ tinytaskapp/lib/main.dart
@@ -4,12 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyDtcrqZDhKdFgPJCJzmhWIA-Ei9oBEHqQQ",
-          appId: "1:202382514585:android:882ade15a361502f24db08",
-          messagingSenderId: "202382514585",
-          projectId: "tinytask-99a5f"));
+  await Firebase.initializeApp();
   runApp(TaskApp());
 }
 
