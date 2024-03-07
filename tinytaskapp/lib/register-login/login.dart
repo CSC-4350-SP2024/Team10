247835e679797470a923018d36e00d568ff3a6fb
kfloyd-gsu <kfloyd20@student.gsu.edu>
2024-03-06 23:02:47 -0500

    color scheme update

--- tinytaskapp/lib/register-login/login.dart
+++ tinytaskapp/lib/register-login/login.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 import 'register.dart';
 
+Color fontColor = Color.fromARGB(255, 255, 255, 255);
+Color backgroundColor = Color.fromARGB(255, 26, 33, 41);
+Color settingsBackgroundColor = Color.fromARGB(255, 37, 55, 73);
+
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: Text('Login', style: TextStyle(color: fontColor)),
+        backgroundColor: backgroundColor, // Applying backgroundColor to the app bar
+        iconTheme: IconThemeData(color: fontColor), 
       ),
       body: Container(
+        color: settingsBackgroundColor, // Applying settingsBackgroundColor to the body
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.mail),
+              decoration: InputDecoration(
+                icon: Icon(Icons.mail, color: fontColor), // Adding fontColor to the icon color
                 labelText: 'Email *',
+                labelStyle: TextStyle(color: fontColor), // Adding fontColor to the label text color
               ),
             ),
             TextFormField(
-              decoration: const InputDecoration(
-                icon: Icon(Icons.lock_person_rounded),
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock_person_rounded, color: fontColor), // Adding fontColor to the icon color
                 labelText: 'Password *',
+                labelStyle: TextStyle(color: fontColor), // Adding fontColor to the label text color
               ),
             ),
             const SizedBox(height: 16.0),
@@ -34,12 +43,12 @@ class LoginScreen extends StatelessWidget {
                 // TODO: Implement sign up logic
               },              
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 37, 55, 73)),
+                backgroundColor: MaterialStateProperty.all(settingsBackgroundColor), // Using settingsBackgroundColor for button background
               ),
-              child: const Text(
+              child: Text(
                 'Sign Up',
                 style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
+                  color: fontColor, // Adding fontColor to button text color
                   fontFamily: 'Roboto',
                 ),
               ),
@@ -47,15 +56,15 @@ class LoginScreen extends StatelessWidget {
             const SizedBox(height: 16.0),
             GestureDetector(
               onTap: () {
-                // Navigate to the sign-in page
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => RegScreen()),
                 );
               },
-              child: const Text(
+              child: Text(
                 "Don't have an account? Register now.",
-              )
+                style: TextStyle(color: fontColor), // Adding fontColor to the text color
+              ),
             )
           ],
         ),
